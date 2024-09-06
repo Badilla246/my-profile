479cba8888addc3c071dc82f222bea99db1aeef6
Badilla246 <badillaangelo45@gmail.com>
2024-09-06 08:08:36 +0800

    first commit

---
+++ src/constants/common.ts
@@ -0,0 +1,15 @@
+export const NAME = 'Angelo D. Badilla';
+
+// ABOUT PAGE
+export const ABOUT_TEXT_1 =
+  'Creative and versatile professional with expertise in video editing and digital product creation for children. As a freelance video editor, I excel in post-production tasks like color grading, sound design, and motion graphics, ensuring high-quality, client-tailored content. In addition to video editing, I design engaging digital products for kids, combining creativity with a deep understanding of what appeals to young audiences. Known for managing projects independently and consistently meeting tight deadlines while delivering exceptional results.';
+export const ABOUT_TEXT_2 =
+  'Responsible for editing videos from start to finish, managing tasks from initial concept to final output. Work closely with clients and creative teams to design, implement, and deliver high-quality visual content and digital products that align with client objectives and engage target audiences.';
+export const ABOUT_TEXT_3 =
+  'Versatile professional skilled in both video editing and digital product creation, capable of managing projects from concept to completion. Possess a strong understanding of various editing software, creative tools, and techniques to deliver engaging and high-quality content.';
+
+// RESUME PAGE
+export const RESUME_TEXT_1 =
+  'Creative and detail-oriented freelance video editor with a strong background in crafting engaging visual stories. Proficient in various editing software, I excel in post-production processes, including color grading, sound design, and motion graphics. Committed to delivering high-quality content tailored to client needs, with a keen eye for detail and a passion for visual storytelling. Experience in editing diverse projects, from short films to promotional videos. Adept at managing projects independently and meeting tight deadlines.';
+export const RESUME_TEXT_2 =
+  'Software Developer with 12+ years of experience building websites and web applications using modern JavaScript  tools/frameworks.';
